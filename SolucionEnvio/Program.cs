a650c99bd65aa0c1ec576e4c53e3cde019e0dc42
yeisonf194 <yeisonf19@gmail.com>
2019-05-14 05:17:43 -0500

    Adelanto 6

--- SolucionEnvio/Program.cs
+++ SolucionEnvio/Program.cs
@@ -1,12 +1,114 @@
 ﻿using System;
+using System.Linq;
 
-namespace SolucionEnvio
+namespace Delegados
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Delegados
+            //Se definen con func<int,int> donde el primer valor es el que retorna, y los siguientes los parametros que recibe
+            //Un delegado siempr retorna algo
+            //Func<double, double> exp = CalcularExponente;
+            //Console.WriteLine($"Resultado: {exp}");
+
+            //Func<int,int> doble
+
+
+            Proyecto project = new Proyecto();
+            project.proyectos = project.AgregarProyecto2();
+            var proys = project.proyectos;
+
+
+
+
+
+            //Consultas con LinQ
+            //Una consulta no hace sobre la clase sino sobre la lista que tenga los datos
+
+
+
+            //1.Obtener todos los proyectos cuya duracion sea mayor a 6 y menor a 12
+            var rango = proys.Where(x => x.Duracion >= 6 && x.Duracion <= 12).ToList();
+            //project.ImprimirProyectos(rango);
+
+
+
+
+
+
+            //2.Obtener todos los proyectos que empiezen por b
+            var projectsB = from p in proys
+                            where (p.Nombre.StartsWith("B"))
+                            select p;
+            //project.ImprimirProyectos(projectsB.ToList());
+
+
+
+
+
+
+            //3.Obtener todos los proyectos cuyo codigo sea multiplo de 7
+            var multiplos = proys.Where(x => x.Codigo % 7 == 0).ToList();
+            //project.ImprimirProyectos(multiplos);
+
+
+
+
+
+
+            //4.Agrupar los proyectos que tengan duracion inferior a seis superior a seis y menor a diez y los que sean mayores a 10
+            var gruposProy = proys.GroupBy(x => {
+                if (x.Duracion < 6) {
+                    return "Son menores que 6";
+                } else if(x.Duracion >= 6 && x.Codigo<10){
+                    return "Estan entre 6 y 10";
+                } else if (x.Duracion >= 10) {
+                    return "Es mayor que 10";
+                } else {
+                    return "No hay datos en este rango";
+                }
+            });
+            //foreach (var agrupando in gruposProy) {
+            //    Console.WriteLine($"Grupo de proyectos {agrupando.Key}");
+            //    foreach(var item in agrupando) {
+            //        Console.WriteLine($"***El nombre del proyecto {item.Nombre}***");
+            //        Console.WriteLine($"La duracion del proyecto {item.Duracion}");
+            //        Console.WriteLine("-");
+            //    }
+            //}
+
+
+
+
+            //5.Obtener el promedio de la duracion de todos los proyectos
+            var promedio = proys.Average(x => x.Duracion);
+            //Console.WriteLine($"El promedio de los proyectos es: {promedio}");
+
+
+
+            //6.Obtener el nombre del proyeccto con mas duracion
+            var maximo = proys.Max(x => x.Duracion);
+            var maxNombre = (from p in proys
+                             where (p.Duracion == maximo)
+                             select p.Nombre).FirstOrDefault();
+            Console.WriteLine($"El maximo de todos los proyectos es: {maxNombre}");
+
+
+
+
+            //7.Seleccionar el nombre y el area del proyecto con duracion mayor a seis, con firtsordefault
+            var mayor6 = proys.Where(x => x.Duracion > 6).ToList();
+
+
+
+
+            //8.Verificar si existe un proyecto con una duracion mayor a 24 con any
+            //9.Sumar las duraciones de los proyectos cuyo codigo sea superior a 20
+            //10.Tomar los 2 primeros proyectos cuyo codigo sea superior a 30
+
+
         }
     }
 }
---
+++ SolucionEnvio/Proyecto.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegados
+{
+    class Proyecto
+    {
+        public List<Proyecto> proyectos = new List<Proyecto>();
+
+        public string Nombre { get; set; }
+        public double Codigo { get; set; }
+        public string Area { get; set; }
+        public int Duracion { get; set; }
+
+        public List<Proyecto> AgregarProyecto()
+        {
+            Console.WriteLine("Escriba Y para ingresar");
+            var res = Console.ReadLine();
+            while (res == "Y")
+            {
+                Console.WriteLine("Escriba el nombre del proyecto");
+
+                var nom = Console.ReadLine();
+                Console.WriteLine("Escriba el codigo del proyecto");
+                var cod = Console.ReadLine();
+                var miProyecto = new Proyecto()
+                {
+                    Nombre = nom,
+                    Codigo = int.Parse(cod)
+                };
+                proyectos.Add(miProyecto);
+                Console.WriteLine("Escriba Y para ingresar");
+                res = Console.ReadLine();
+            }
+            return proyectos;
+        }
+        public List<Proyecto> AgregarProyecto2()
+        {
+            var proys = new List<Proyecto>() {
+                new Proyecto()
+                {
+                    Nombre = "Bancaria",
+                    Codigo = 23,
+                    Area = "Comercio",
+                    Duracion = 8
+                },
+                new Proyecto()
+                {
+                    Nombre = "Mercadeo",
+                    Codigo = 23,
+                    Area = "Talento Humano",
+                    Duracion = 11
+                },
+                new Proyecto()
+                {
+                    Nombre = "Atencion Primaria",
+                    Codigo = 7,
+                    Area = "AIPI",
+                    Duracion = 18
+                },
+                new Proyecto()
+                {
+                    Nombre = "Adsi",
+                    Codigo = 32,
+                    Area = "Sistemas",
+                    Duracion = 24
+                },
+                new Proyecto()
+                {
+                    Nombre = "Gastronomia",
+                    Codigo = 29,
+                    Area = "Postres",
+                    Duracion = 10
+                }
+            };
+            return proys;
+        }
+        public void ImprimirProyectos(List<Proyecto> proy)
+        {
+            foreach (var item in proy)
+            {
+                Console.WriteLine($"***{item.Nombre}***");
+                Console.WriteLine($"__{item.Codigo}_");
+                Console.WriteLine($"{item.Area}");
+                Console.WriteLine($"{item.Duracion}");
+                Console.WriteLine($"-");
+            }
+
+
+
+            //Obtener todos los proyectos cuya duracion sea mayor a 6 y menor a 12
+            //Obtener todos los proyectos que empiezen por b
+            //Obtener todos los proyectos cuyo codigo sea multiplo de 7
+            //Agrupar los proyectos que tengan duracion inferior a seis superior a seis y menor a diez y los que sean mayores a 10
+            //Obtener el promedio de la duracion de todos los proyectos
+            //Obtener el nombre del proyeccto con mas duracion
+            //Seleccionar el nombre y el area del proyecto con duracion mayor a seis, con firtsordefault
+            //Verificar si existe un proyecto con una duracion mayor a 24 con any
+            //Sumar las duraciones de los proyectos cuyo codigo sea superior a 20
+            //Tomar los 2 primeros proyectos cuyo codigo sea superior a 30
+        }
+    }
+}
